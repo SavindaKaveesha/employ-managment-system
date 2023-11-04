bc03d49df347603ed7dbdeebbaa6a25e90a23ff8
SavindaKaveesha <kaveeshasavinda98@gmail.com>
2023-11-04 21:35:11 +0530

    interface complete

--- employ managment system/Form1.Designer.cs
+++ employ managment system/Form1.Designer.cs
@@ -30,7 +30,14 @@
         {
             panel1 = new Panel();
             label1 = new Label();
+            panel2 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -46,22 +53,74 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(301, 21);
+            label1.Location = new Point(216, 20);
             label1.Name = "label1";
             label1.Size = new Size(567, 50);
             label1.TabIndex = 0;
             label1.Text = "Employee Management System";
             // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(12, 110);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(212, 393);
+            panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(38, 164);
+            button2.Name = "button2";
+            button2.Size = new Size(136, 58);
+            button2.TabIndex = 1;
+            button2.Text = "Employe Information";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(38, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Employe";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.caucasian_female_singer_portrait_isolated_neon;
+            pictureBox1.Location = new Point(230, 113);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(787, 390);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(38, 276);
+            button3.Name = "button3";
+            button3.Size = new Size(136, 58);
+            button3.TabIndex = 2;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1152, 450);
+            ClientSize = new Size(1029, 510);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -69,5 +128,10 @@
 
         private Panel panel1;
         private Label label1;
+        private Panel panel2;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button3;
     }
 }
---
+++ employ managment system/employe.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace employ_managment_system
+{
+    partial class employe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1012, 102);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(398, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Employe";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.AppWorkspace;
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 120);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1012, 360);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Employe";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(30, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 28);
+            label2.TabIndex = 0;
+            label2.Text = "ID";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(30, 289);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 28);
+            label3.TabIndex = 1;
+            label3.Text = "Benifit";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(30, 249);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 28);
+            label4.TabIndex = 2;
+            label4.Text = "DOB";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(30, 208);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 28);
+            label5.TabIndex = 3;
+            label5.Text = "Salary";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(30, 166);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 28);
+            label6.TabIndex = 4;
+            label6.Text = "Email";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(30, 126);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 28);
+            label7.TabIndex = 5;
+            label7.Text = "Age";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(30, 88);
+            label8.Name = "label8";
+            label8.Size = new Size(166, 28);
+            label8.TabIndex = 6;
+            label8.Text = "Employee Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(231, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(277, 31);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(231, 286);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(277, 31);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(231, 246);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(277, 31);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(231, 205);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(277, 31);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(231, 165);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(277, 31);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(231, 123);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(277, 31);
+            textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.Location = new Point(231, 84);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(277, 31);
+            textBox7.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(651, 45);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(251, 215);
+            pictureBox1.TabIndex = 14;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(576, 289);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 47);
+            button1.TabIndex = 15;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(864, 286);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 47);
+            button2.TabIndex = 16;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(722, 289);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 47);
+            button3.TabIndex = 17;
+            button3.Text = "New";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 494);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1012, 188);
+            dataGridView1.TabIndex = 2;
+            // 
+            // employe
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1036, 696);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            Name = "employe";
+            Text = "employe";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+    }
+}
